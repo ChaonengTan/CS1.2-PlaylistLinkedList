6354bef092d47772c2e636d3b1f08219cc0d5b76
Chaoneng Tan <69783753+ChaonengTan@users.noreply.github.com>
2021-01-25 22:50:05 -0800

    WIP

--- Playlist.py
+++ Playlist.py
@@ -8,14 +8,22 @@ class Playlist:
   # TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
 
   def add_song(self, title):
-    pass
-
+    newSong = Song(title)
+    newSong.set_next_song = self.__first_song
+    self.__first_song = newSong
 
 
   # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
 
   def find_song(self, title):
-    pass
+    current_node = self.__first_song
+    counter = 0 
+    while current_node.get_next_song != None:
+      counter = counter + 1
+      if current_node.get_title == title:
+        return counter
+      current_node = current_node.get_next_song
+      
 
 
   # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
@@ -28,7 +36,12 @@ class Playlist:
   # TODO: Create a method called length, which returns the number of songs in the playlist.
 
   def length(self):
-    pass
+    current_node = self.__first_song
+    counter = 0 
+    while current_node.get_next_song != None:
+      counter = counter + 1
+      current_node = current_node.get_next_song
+    return counter
 
 
   # TODO: Create a method called print_songs that prints a numbered list of the songs in the playlist.
@@ -39,6 +52,11 @@ class Playlist:
   # 3. Song Title 3
 
   def print_songs(self):
-    pass
+    current_node = self.__first_song
+    counter = 0 
+    while current_node.get_next_song != None:
+      counter = counter + 1
+      print(f'{counter}. {current_node.get_title}')
+      current_node = current_node.get_next_song
 
   
