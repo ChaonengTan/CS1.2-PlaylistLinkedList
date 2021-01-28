8148c8bf73281a35aca9a90b941cd58a6fc53f17
Chaoneng Tan <69783753+ChaonengTan@users.noreply.github.com>
2021-01-27 22:26:59 -0800

    Running blind

--- Playlist.py
+++ Playlist.py
@@ -2,43 +2,56 @@ from Song import Song
 
 class Playlist:
   def __init__(self):
-    self._first_song = None
+    self.__first_song = None
 
 
   # TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
 
   def add_song(self, title):
     newSong = Song(title)
-    newSong.set_next_song = self._first_song
-    self._first_song = newSong
+    newSong.set_next_song = self.__first_song
+    self.__first_song = newSong
 
 
   # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
 
   def find_song(self, title):
-    current_node = self._first_song
+    current_node = self.__first_song
     counter = 0 
-    while current_node.get_next_song != None:
-      counter = counter + 1
+    while current_node != None:
       if current_node.get_title == title:
         return counter
+      counter = counter + 1
       current_node = current_node.get_next_song
+    return -1
       
 
 
   # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
 
   def remove_song(self, title):
-    pass
+    if self.__first_song == title:
+        self.__first_song = self.__first_song.get_next_song
+        print('Item Removed')
+        return
+    current_node = self.__first_song
+    def checkTrue(title, current_node):
+      if current_node.get_title == title:
+        return True
+      current_node = current_node.get_next_song
+      if checkTrue(title, current_node):
+        current_node.set_next_song = current_node.get_next_song.get_next_song
+        print('Item Removed')
+    checkTrue(title, current_node)
 
 
 
   # TODO: Create a method called length, which returns the number of songs in the playlist.
 
   def length(self):
-    current_node = self._first_song
+    current_node = self.__first_song
     counter = 0 
-    while current_node.get_next_song != None:
+    while current_node != None:
       counter = counter + 1
       current_node = current_node.get_next_song
     return counter
@@ -52,11 +65,11 @@ class Playlist:
   # 3. Song Title 3
 
   def print_songs(self):
-    current_node = self._first_song
+    current_node = self.__first_song
     counter = 0 
-    while current_node.get_next_song != None:
+    while current_node != None:
+      print(f'{counter + 1}. {current_node.get_title}')
       counter = counter + 1
-      print(f'{counter}. {current_node.get_title}')
       current_node = current_node.get_next_song
 
   
--- Song.py
+++ Song.py
@@ -1,39 +1,39 @@
 class Song:
 
   def __init__(self, title):
-      self._title = title
-      self._next_song = None
+      self.__title = title
+      self.__next_song = None
 
 
   # TODO: Create a getter method for the title attribute, called get_title
   def get_title(self):
-    return self._title
+    return self.__title
     
   
   # TODO: Create a setter method for the next_song attribute, called set_title. Make sure titles are type cased to strings and are Title Cased.
   def set_title(self, title):
-    self._title = title
+    self.__title = title
 
 
   # TODO: Create a getter method for the next_song attribute, called get_next_song
   def get_next_song(self):
-    return self._next_song
+    return self.__next_song
 
 
   # TODO: Create a setter method for the next_song attribute, called set_next_song
   def set_next_song(self, next_title):
-    self._next_song = next_title
+    self.__next_song = next_title
 
 
   # TODO: Using the __str___ dunder method, return a string of the song title.
   def __str__(self):
-    return str(self._title)
+    return str(self.__title)
 
 
   # TODO: Using the __repr__ dunder method, return a string formatted as the following:'Song Title -> Next Song Title'
   def __repr__(self):
-    return str(f'{self.get_title} -> {self.get_next_song.get_title}')
+    return f'{self.get_title} -> {self.get_next_song}'
+
 
 song = Song('yes')
-print(song._title)
 print(song.get_title)
